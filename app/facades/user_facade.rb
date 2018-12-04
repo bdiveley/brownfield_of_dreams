e54a8c2e9b80517bd1c7167fa375a7571cff7a21
Cesar Jolibois <diazcd85@gmail.com>
2018-12-04 14:27:31 -0700

    WIP create facade, migration,

---
+++ app/facades/user_facade.rb
@@ -0,0 +1,23 @@
+class UserFacade
+
+  def initialize(data)
+    @token = data.token
+    @_search_repo_result = nil
+  end
+
+  def repos
+    search_repo_result.map do |repo_data|
+      Repo.new(repo_data)
+    end 
+  end
+
+  private
+
+  def search_repo_result
+    @_search_repo_result ||= service.repo_list
+  end
+  def service
+    GitHubService.new({token: @token})
+  end
+
+end
