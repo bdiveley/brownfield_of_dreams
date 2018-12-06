f4177b5faaa4b75f38f0ed53e1673de9b20e8171
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-06 16:13:30 -0700

    Fixed update method by allowing updates without password in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password, on: :create
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
@@ -11,5 +11,6 @@ class User < ApplicationRecord
   def find_or_create_from_auth_hash(user, auth_hash)
     token = auth_hash["credentials"]["token"]
     user.update(token: token)
+    user.save
   end
 end
