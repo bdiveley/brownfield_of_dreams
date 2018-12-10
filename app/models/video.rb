fda93496be6f0dbe92e1ea062f70db8a73bbec15
Cesar Jolibois <diazcd85@gmail.com>
2018-12-10 15:12:45 -0700

    WIP test finish

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,11 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  def self.user_videos(user)
+    joins(:user_videos)
+    .order(:tutorial_id, :position)
+    .where("user_videos.user_id = #{user.id}")
+    require "pry"; binding.pry
+  end
 end
--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -58,7 +58,7 @@ describe 'User' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit friendship_path(36523333)
-    save_and_open_page
+
     expect(page).to have_content("Cannot add friends with unlinked GitHub accounts")
     expect(current_path).to eq(dashboard_path)
   end
