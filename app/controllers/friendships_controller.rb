e4d3d2b0731f879d822c7480220c5addccaf442f
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-10 11:41:10 -0700

    UPdate .build method to .create in friendship create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,13 @@
 class FriendshipsController < ApplicationController
 
   def create
+    binding.pry
     friend = User.find_by(github_id: params[:friend_id])
     if friend === nil
       flash[:error] = "Cannot add friends with unlinked GitHub accounts"
       redirect_to dashboard_path
     else
-      friendship = current_user.friendships.build(:friend_id => friend.id)
+      friendship = current_user.friendships.create(:friend_id => friend.id)
       redirect_to dashboard_path
     end
   end
