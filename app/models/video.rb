db0a2f112f3790b2bfc3f4043e96ff3bec143f57
Cesar Jolibois <diazcd85@gmail.com>
2018-12-10 16:00:23 -0700

    fixed rubocop warning and limit user_video class method to 8

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   validates_presence_of :title
-  
+
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
@@ -9,5 +9,6 @@ class Video < ApplicationRecord
     joins(:user_videos)
     .order(:tutorial_id, :position)
     .where("user_videos.user_id = #{user.id}")
+    .limit(8)
   end
 end
