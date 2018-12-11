15b328855c10dfeba721dddf11ed1ea76c403597
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-11 10:03:19 -0700

    Add additional logic to tutorial*#show to redirect visitors from specific tutorial show pages

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,7 +1,7 @@
 class TutorialsController < ApplicationController
   def show
     filtered_tutorials = Tutorial.filter_tutorials(current_user)
-    if !filtered_tutorials.empty?
+    if !filtered_tutorials.empty? &&  current_user
       tutorial = filtered_tutorials.find(params[:id])
       @facade = TutorialFacade.new(tutorial, params[:video_id])
     else
