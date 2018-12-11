5d5c2736e403a7b8c6afafca8240c6564b1a478f
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-11 10:40:36 -0700

    Fix test failures on visitor access to tutroials on root path

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,8 +1,12 @@
 class TutorialsController < ApplicationController
   def show
     filtered_tutorials = Tutorial.filter_tutorials(current_user)
-    if !filtered_tutorials.empty? &&  current_user
-      tutorial = filtered_tutorials.find(params[:id])
+
+    tutorial = filtered_tutorials.find do |tutorial|
+      tutorial.id == params[:id].to_i
+    end
+
+    if tutorial
       @facade = TutorialFacade.new(tutorial, params[:video_id])
     else
       redirect_to root_path
