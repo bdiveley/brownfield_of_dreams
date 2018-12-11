216013580b13fedd4f81cf81d74312b4d16f90f3
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-10 18:19:07 -0700

    Add restriction that visitors cannot see all tutorials on root path

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    tutorials = Tutorial.filter_tutorials(current_user)
     if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+      @tutorials = tutorials.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
     else
-      @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
+      @tutorials = tutorials.paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,13 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+  def self.filter_tutorials(user)
+    if user
+      Tutorial.all
+    else
+      Tutorial.where(classroom: false)
+    end
+  end
+
 end
