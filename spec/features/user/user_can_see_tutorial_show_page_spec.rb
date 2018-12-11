fb5cf57b5d843539b868afc1484f71be58ce684c
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-11 11:59:07 -0700

    FIx error on tutorial show page when no videos are associated with that tutorial

--- spec/features/user/user_can_see_tutorial_show_page_spec.rb
+++ spec/features/user/user_can_see_tutorial_show_page_spec.rb
@@ -13,4 +13,16 @@ describe 'User' do
     expect(page).to have_content(video.title)
 
   end
+  it 'does not get an error if the tutorial has no associated videos' do
+    user = create(:user)
+    tutorial = create(:tutorial, id: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/tutorials/1'
+
+    expect(page).to have_content(tutorial.title)
+    expect(current_path).to eq(tutorial_path(tutorial))
+
+  end
 end
