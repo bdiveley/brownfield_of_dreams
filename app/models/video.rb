8de85abd9a89bda3e8b97ba32a26b8474e779f57
Cesar Jolibois <diazcd85@gmail.com>
2018-12-12 15:33:31 -0700

    create rake tasks without test

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   validates_presence_of :title, :position
-  validates :position, numericality: {greater_than_or_equal_to: 0}
+  validates :position, on: :create, numericality: {greater_than_or_equal_to: 0}
 
   has_many :user_videos
   has_many :users, through: :user_videos
