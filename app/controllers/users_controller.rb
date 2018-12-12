c70dc9f08eb2af1e2cb1220e5945eeb69c45bf0b
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-12 16:01:32 -0700

    Added flash messages to dashboard page after a visitor has registered for an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:notice] = "Logged in as #{user.first_name} #{user.last_name}"
+      flash[:error] = "This account has not yet been activated. Please check your email."
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'vister can create an account', :js do
   it ' visits the home page' do
     stub_github_api_call
-    
+
     email = 'jimbob@aol.com'
     first_name = 'Jim'
     last_name = 'Bob'
@@ -35,5 +35,34 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
 
+  end
+  it ' visits the root and registers' do
+    stub_github_api_call
+
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit '/'
+
+    click_on 'Register'
+
+    expect(current_path).to eq(register_path)
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on'Create Account'
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("Logged in as #{first_name} #{last_name}")
+
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
   end
 end
