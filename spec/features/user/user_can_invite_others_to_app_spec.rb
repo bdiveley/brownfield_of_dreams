a26a44d040109871ae553dd281767aae56f6b10a
Cesar Jolibois <diazcd85@gmail.com>
2018-12-13 14:35:24 -0700

    create edge case for User invites test

--- spec/features/user/user_can_invite_others_to_app_spec.rb
+++ spec/features/user/user_can_invite_others_to_app_spec.rb
@@ -1,24 +1,54 @@
 require 'rails_helper'
 
 feature"User invites" do
-  scenario "user can invite others to the app " do
-    # As a registered user
+  before(:each) do
+    stub_github_api_call
+    stub_github_followers_api_call
+    stub_github_following_api_call
+
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    # When I visit /dashboard
+  end
+
+  scenario "user can invite others to the app " do
+    stub_github_invite_api_call
+
     visit dashboard_path
-    # And I click "Send an Invite"
+
     click_button("Send an Invite")
-    # Then I should be on /invite
+
     expect(current_path).to eq(invite_path)
-    # And when I fill in "Github Handle" with <A VALID GITHUB HANDLE>
+
     fill_in 'handle', with: "bdiveley"
-    # And I click on "Send Invite"
+
     click_on("Send Invite")
-    # Then I should be on /dashboard
+
+    mail = ActionMailer::Base.deliveries
+
+    expect(mail.count).to eq(1)
+
+    expect(mail.first.body.encoded).to match("Here")
+
     expect(current_path).to eq(dashboard_path)
 
     expect(page).to have_content("Successfully sent invite!")
+  end
 
-    end
+  scenario "user type nonexistent handle " do
+    stub_github_invite_not_found_api_call
+
+    visit dashboard_path
+
+    click_button("Send an Invite")
+
+    expect(current_path).to eq(invite_path)
+
+    fill_in 'handle', with: "El_Vago_de_al_lado"
+
+    click_on("Send Invite")
+
+    expect(current_path).to eq(dashboard_path)
+
+    expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+  end
 end
