b140b9cbb85dd7701bbb245ffca16d1e4b842e2f
Bailey Diveley <bailey.diveley@gmail.com>
2018-12-13 10:12:37 -0700

    Add validates presence of last name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
-  validates_presence_of :first_name
+  validates_presence_of :first_name, :last_name
   enum role: [:default, :admin]
   has_secure_password
 
